e7d4438985c4cb9f758b329dd8934dca8e3f6034
Stalius4 <deivydasrimgaila@gmail.com>
2023-04-10 18:21:06 +0100

    logo added

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,34 +1,28 @@
 
 import './menu.css';
+import logo from "./logo.svg"
 function Menu() {
 
 
-  return ( <div className='background'>
-    <div className='logo'>
-        <div>Rumblings to-go</div>
-<div className='logo-p'>Order from<spam className="red-text">7:30am till 2:45pm</spam> for collection to suit you<spam className="red-text"> (01775) 722014 </spam></div>
-    </div>
+  return ( 
+  
+  <div>
+       <header className='header'>
+        <div className='logo'>
+          {/* <div className='top-line'></div> */}
+          <img src={logo} alt="logo" className='logo-img' />
+          <div className='under-logo-text'>Order from <spam className="red-text"> 7.30am till 2.45pm</spam> for collection to suit you<spam className="red-text"> (01775) 722014</spam>  </div>
+        </div> 
+    
+    </header>
+    <main>
+    
+    </main>
+    <footer>
+     
+    </footer>
 
-<div >
-<ul className='food-categories'>
-  <li>Breakfast</li>
-  <li>Cheese melt baguettes</li>
-  <li>Jacket potatoes</li>
-  <li>Toasties</li>
-  <li>Sandwiches, rolls, wraps & baguettes</li>
-  <li>Salad boxes</li>
-  <li>Drinks</li>
-</ul>
-</div>
-<div className='description'>
-    <h1>Breakfast</h1>
-    <p>Served all day, choose your first item bacon, sausage or egg then...</p>
-    <div  className='breakfast-choice'>
-        <div className='breakfast-box'>Sliced white or brown bread.. £2.95</div>
-        <div className='breakfast-box'>Soft white rolls or baguettes £3.50</div>
-        <div className='breakfast-box'>Breakfast box £1.50</div>
-    </div>
-</div>
+
     </div>
   );
 }
