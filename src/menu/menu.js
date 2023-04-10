1860308620fa47c54fb8491ed1f60f057873e8d4
Stalius4 <deivydasrimgaila@gmail.com>
2023-04-10 19:29:29 +0100

    logo added

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -11,7 +11,7 @@ function Menu() {
         <div className='logo'>
           {/* <div className='top-line'></div> */}
           <img src={logo} alt="logo" className='logo-img' />
-          <div className='under-logo-text'>Order from <spam className="red-text"> 7.30am till 2.45pm</spam> for collection to suit you<spam className="red-text"> (01775) 722014</spam>  </div>
+          <div className='under-logo-text'>Order from <spam className="red-text"> 7.30am till 2.45pm</spam> for collection to suit you<spam className="red-text"> (01775)722014</spam>  </div>
         </div> 
     
     </header>
